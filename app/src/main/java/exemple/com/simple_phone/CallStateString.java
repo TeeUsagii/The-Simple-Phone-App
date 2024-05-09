9ecd6c87ce6f2799b2091082abb3a948285acf59
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-05-09 11:53:07 +0700

    first commit

---
+++ app/src/main/java/exemple/com/simple_phone/CallStateString.java
@@ -0,0 +1,48 @@
+package exemple.com.simple_phone;
+
+import android.annotation.SuppressLint;
+
+import timber.log.Timber;
+
+final class CallStateString {
+    @SuppressLint("TimberArgCount")
+    static String asString(int $receiver) {
+        String state;
+        switch($receiver) {
+            case 0:
+                state = "Gọi mới";
+                break;
+            case 1:
+                state = "Đang gọi";
+                break;
+            case 2:
+                state = "Cuộc gọi đến";
+                break;
+            case 3:
+                state = "Giữ máy";
+                break;
+            case 4:
+                state = "Đã kết nối";
+                break;
+            case 5:
+            case 6:
+            default:
+                Timber.w("Unknown state %s", $receiver, new Object[0]);
+                state = "UNKNOWN";
+                break;
+            case 7:
+                state = "Đã ngắt kết nối";
+                break;
+            case 8:
+                state = "SELECT_PHONE_ACCOUNT";
+                break;
+            case 9:
+                state = "Đang kết nối";
+                break;
+            case 10:
+                state = "Đang ngắt kết nối";
+        }
+
+        return state;
+    }
+}
---
+++ app/src/main/java/exemple/com/simple_phone/DialerActivity.java
@@ -0,0 +1,84 @@
+package exemple.com.simple_phone;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telecom.TelecomManager;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.ArrayList;
+
+import static android.Manifest.permission.CALL_PHONE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.telecom.TelecomManager.ACTION_CHANGE_DEFAULT_DIALER;
+import static android.telecom.TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME;
+
+
+public class DialerActivity extends AppCompatActivity {
+    EditText phoneNumberInput;
+    private static final int REQUEST_PERMISSION = 0;
+
+    Button callButton; // Khai báo nút gọi
+
+        @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialer);
+
+        phoneNumberInput = findViewById(R.id.phoneNumberInput);
+        callButton = findViewById(R.id.callButton);
+        // get Intent data (tel number)
+        if (getIntent().getData() != null)
+            phoneNumberInput.setText(getIntent().getData().getSchemeSpecificPart());
+
+            callButton.setOnClickListener(v -> makeCall());
+        }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        offerReplacingDefaultDialer();
+
+        phoneNumberInput.setOnEditorActionListener((v, actionId, event) -> {
+            makeCall();
+            return true;
+        });
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void makeCall() {
+        // If permission to call is granted
+        if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
+            // Create the Uri from phoneNumberInput
+            Uri uri = Uri.parse("tel:"+phoneNumberInput.getText());
+            // Start call to the number in input
+            startActivity(new Intent(Intent.ACTION_CALL, uri));
+        } else {
+            // Request permission to call
+            ActivityCompat.requestPermissions(this, new String[]{CALL_PHONE}, REQUEST_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        ArrayList<Integer> grantRes = new ArrayList<>();
+        // Add every result to the array
+        for (Integer grantResult: grantResults) grantRes.add(grantResult);
+
+        if (requestCode == REQUEST_PERMISSION && grantRes.contains(PERMISSION_GRANTED)) {
+            makeCall();
+        }
+    }
+
+    private void offerReplacingDefaultDialer() {
+        TelecomManager systemService = this.getSystemService(TelecomManager.class);
+        if (systemService != null && !systemService.getDefaultDialerPackage().equals(this.getPackageName())) {
+        startActivity((new Intent(ACTION_CHANGE_DEFAULT_DIALER)).putExtra(EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, this.getPackageName()));
+        }
+    }
+}
